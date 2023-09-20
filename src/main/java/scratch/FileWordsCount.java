c8e075794f675d96bc08446a6cfffb75a620467d
Ishan Pandya <ishanpandya10@gmail.com>
2023-09-20 20:41:55 +0100

    Modified a word count program to print a map having the word with maximum number of occurrence

--- src/main/java/scratch/FileWordsCount.java
+++ src/main/java/scratch/FileWordsCount.java
@@ -3,6 +3,7 @@ package scratch;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -11,17 +12,22 @@ import java.util.stream.Stream;
 
 public class FileWordsCount {
     public static void main(String[] args) throws IOException {
-        List<String> lines = Files.readAllLines(Paths.get("E:\\Projects\\scratchpad\\src\\main\\resources\\README.txt"));
+        List<String> lines = Files.readAllLines(Paths.get("src\\main\\resources\\README.txt"));
 
         Map<String, Long> wordCounts = lines.parallelStream()
                 .flatMap(line -> Stream.of(line.split("\\s")))
                 .map(String::trim) //Remove unnecessary spaces from the word
                 .map(String::toLowerCase)
-                .map(word -> word.replaceAll("[^A-Za-z0-9]",""))
-                .filter(word -> word.length() > 0) //Remove blank words
+                .map(word -> word.replaceAll("[^A-Za-z0-9]", ""))
+                .filter(word -> !word.isEmpty()) //Remove blank words
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        System.out.println(wordCounts);
+        LinkedHashMap<String, Long> sortedMap = wordCounts.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o, o2) -> o, LinkedHashMap::new));
+
+        System.out.println(sortedMap);
 
     }
 }
