32b504efd4b18f38bd124d171b3f033a68cc0f9f
Ishan Pandya <ishanpandya10@gmail.com>
2023-12-05 23:51:12 +0000

    Advent Day 1 completed

--- src/main/java/scratch/advent2023/Day1.java
+++ src/main/java/scratch/advent2023/Day1.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day1 {
     public static void main(String[] args) throws IOException {
@@ -19,17 +22,73 @@ public class Day1 {
     }
 
     private static int getFirstAndLastNumber(String line) {
+        line = convertStringNumberToDigits(line);
         char[] charArray = line.toCharArray();
         List<Integer> digits = new ArrayList<>();
-        for(char ch: charArray){
-            if(Character.isDigit(ch)){
+        for (char ch : charArray) {
+            if (Character.isDigit(ch)) {
                 digits.add(Integer.parseInt(String.valueOf(ch)));
             }
         }
-        if(digits.isEmpty())
+        if (digits.isEmpty())
             return 0;
-        else if(digits.size() > 1)
+        else if (digits.size() > 1)
             return Integer.parseInt(digits.get(0) + "" + digits.get(digits.size() - 1));
         return Integer.parseInt(digits.get(0) + "" + digits.get(0));
+    }
+
+    private static String convertStringNumberToDigits(String line) {
+        Map<Integer, String> finalMap = new HashMap<>();
+        Map<String, Integer> stringDigitMap = getStringDigitMap();
+        char[] lineCharArray = line.toCharArray();
+
+        for (char ch : lineCharArray) {
+            if (Character.isDigit(ch)) {
+                List<Integer> allIndex = getAllIndex(line, ch + "");
+                for (int idx : allIndex) {
+                    finalMap.put(idx, String.valueOf(ch));
+                }
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : stringDigitMap.entrySet()) {
+            List<Integer> allIndex = getAllIndex(line, entry.getKey());
+            if (!allIndex.isEmpty()) {
+                for (int idx : allIndex) {
+                    finalMap.put(idx, String.valueOf(entry.getValue()));
+                }
+            }
+        }
+
+        List<String> res = finalMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue)
+                .toList();
+
+        return String.join("", res);
+    }
+
+    private static List<Integer> getAllIndex(String line, String guess) {
+        List<Integer> integerList = new ArrayList<>();
+        int index = line.indexOf(guess);
+        while (index >= 0) {
+            integerList.add(index);
+            index = line.indexOf(guess, index + 1);
+        }
+        return integerList;
+    }
+
+    private static Map<String, Integer> getStringDigitMap() {
+        Map<String, Integer> stringDigit = new LinkedHashMap<>();
+        stringDigit.put("one", 1);
+        stringDigit.put("two", 2);
+        stringDigit.put("three", 3);
+        stringDigit.put("four", 4);
+        stringDigit.put("five", 5);
+        stringDigit.put("six", 6);
+        stringDigit.put("seven", 7);
+        stringDigit.put("eight", 8);
+        stringDigit.put("nine", 9);
+        return stringDigit; //55078 //54086 //54623 //54628 //54629 //54610 //54591
     }
 }
